9648b0b19ab3a849aa7ea723b9954d8b4511a3ad
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-09-22 20:21:49 +0300

    Фиксы регистрации и новые иконки в Профиле

--- DragonTest/App/RootTabBarController.swift
+++ DragonTest/App/RootTabBarController.swift
@@ -12,27 +12,53 @@ final class RootTabBarController: UITabBarController {
         super.viewDidLoad()
 
         let profile = UINavigationController(rootViewController: ProfileViewController())
-        profile.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.crop.circle"), tag: 0)
+        profile.tabBarItem = UITabBarItem(title: "Профиль",
+                                          image: UIImage(systemName: "person.crop.circle"),
+                                          tag: 0)
         
         let test = UINavigationController(rootViewController: DragonSelectViewController())
-        test.tabBarItem = UITabBarItem(title: "Тесты", image: UIImage(named: "dragon.icon"), tag: 1)
+        test.tabBarItem = UITabBarItem(title: "Тесты",
+                                       image: UIImage(named: "dragon.icon"),
+                                       tag: 1)
         
         let settings = UINavigationController(rootViewController: SettingsViewController())
-        settings.tabBarItem = UITabBarItem(title: "Настройки", image: UIImage(systemName: "gearshape"), tag: 2)
+        settings.tabBarItem = UITabBarItem(title: "Настройки",
+                                           image: UIImage(systemName: "gearshape"),
+                                           tag: 2)
 
         viewControllers = [profile, test, settings]
         
+        setupTabBarAppearance()
+    }
+    
+    private func setupTabBarAppearance() {
         let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
         
-
-        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(red: 2.05, green: 1.65, blue: 0.72, alpha: 1.0)
-        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor(red: 2.05, green: 1.65, blue: 0.72, alpha: 1.0)]
+        let selectedColor = UIColor(red: 2.05, green: 1.65, blue: 0.72, alpha: 1.0)
+        let normalColor: UIColor = {
+            if traitCollection.userInterfaceStyle == .dark {
+                return .white
+            } else {
+                return .black
+            }
+        }()
         
-        appearance.stackedLayoutAppearance.normal.iconColor = .white
-        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.stackedLayoutAppearance.selected.iconColor = selectedColor
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: selectedColor]
+        
+        appearance.stackedLayoutAppearance.normal.iconColor = normalColor
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: normalColor]
         
         tabBar.standardAppearance = appearance
-        tabBar.scrollEdgeAppearance = appearance
-        
+    }
+    
+    // Чтобы обновлять цвета при смене темы
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if previousTraitCollection?.userInterfaceStyle != traitCollection.userInterfaceStyle {
+            setupTabBarAppearance()
+        }
     }
 }
+
